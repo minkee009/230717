3b28f26add7a82b1dde392b7e19420f6832d5b3d
minkee5004 <minkee5004@gmail.com>
2023-07-18 15:30:47 +0900

    position 구조체 추가하고, 탈출지점 만듬

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositionExtension
+{
+    public struct Position
+    {
+        public int x, y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool isCollideOtherPos(Position other)
+        {
+            return (x == other.x) && (y == other.y);
+        }
+
+        public static Position operator -(Position a, Position b)
+        {
+            return new Position(a.x - b.x, a.y - b.y);
+        }
+        public static Position operator +(Position a, Position b)
+        {
+            return new Position(a.x + b.x, a.y + b.y);
+        }
+
+        public static Position operator *(Position a, int b) 
+        {
+            return new Position(a.x * b, a.y * b);
+        }
+        public static Position operator *(int a, Position b)
+        {
+            return new Position(b.x * a, b.y * a);
+        }
+
+        public static Position operator *(Position a, Position b)
+        {
+            return new Position(a.x * b.x, a.y * b.y);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PositionExtension;
 
 namespace _230717
 {
     internal class Program
     {
-        static int playerX = 0;
-        static int playerY = 0;
-
-        static int enemyX = 0;
-        static int enemyY = 0;
+        static Position PlayerPos = new Position(0,0);
+        static Position EnemyPos = new Position(0,0);
 
         static int[,] map = {
                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, // [0,x]
                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // [1,x]
-               { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // [2,x]
+               { 1, 0, 0, 0, 0, 0, 0, 3, 0, 1 }, // [2,x]
                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // [3,x]
                { 1, 0, 0, 0, 2, 0, 0, 0, 0, 1 },
                { 1, 0, 0, 0, 2, 0, 0, 0, 0, 1 },
@@ -33,32 +32,38 @@ namespace _230717
             while (isNotOverYet)
             {
                 //처음 위치
-                playerX = 5;
-                playerY = 5;
-
-                enemyX = 1;
-                enemyY = 2;
+                PlayerPos = new Position(5, 5);
+                EnemyPos = new Position(1, 2);
 
                 //첫 화면
                 Render();
 
                 ConsoleKeyInfo info = new ConsoleKeyInfo();
-                bool isEndGame = false;
+                int gameStateFlag = 0;
 
-                while (!(info.Key == ConsoleKey.Escape) && !isEndGame)
+                while (!(info.Key == ConsoleKey.Escape) && gameStateFlag == 0)
                 {
-                    ////인풋
+                    //인풋
                     info = Input();
 
-                    ////프로세스
-                    isEndGame = Process(info);
+                    //프로세스
+                    gameStateFlag = Process(info);
 
-                    ////렌더
-                    //화면 그리기
+                    //렌더
                     Console.Clear();
                     Render();
                 }
-                Console.WriteLine("Game Over");
+                switch (gameStateFlag)
+                {
+                    case 1:
+                        Console.WriteLine("Escape Success");
+                        break;
+                    case 2:
+                        Console.WriteLine("Game Over");
+                        break;
+                }
+
+               
                 Delay(1000);
                 Console.WriteLine("Restart ? Y / N");
                 string restartgame = Console.ReadLine();
@@ -89,78 +94,106 @@ namespace _230717
             return Console.ReadKey(true);
         }
 
-        static bool Process(ConsoleKeyInfo info)
+        /// <summary>
+        /// 게임의 전반 로직을 수행하고 게임의 상태를 플래그 값으로 반환한다. // 0 : 계속, 1 : 승리, 2 : 패배
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        static int Process(ConsoleKeyInfo info)
         {
-            int lastPlayerX = playerX;
-            int lastPlayerY = playerY;
+            var lastPlayerPos = new Position(PlayerPos.x, PlayerPos.y);
 
             switch (info.Key)
             {
                 case ConsoleKey.UpArrow:
-                    playerY--;
+                    PlayerPos.y--;
                     break;
                 case ConsoleKey.DownArrow:
-                    playerY++;
+                    PlayerPos.y++;
                     break;
                 case ConsoleKey.LeftArrow:
-                    playerX--;
+                    PlayerPos.x--;
                     break;
                 case ConsoleKey.RightArrow:
-                    playerX++;
+                    PlayerPos.x++;
                     break;
             }
 
-            var lastEnemyX = enemyX;
-            var lastEnemyY = enemyY;
+            //플레이어 위치 제한
+            PlayerPos.x = PlayerPos.x > 0 ? Math.Min(PlayerPos.x, 9) : 0;
+            PlayerPos.y = PlayerPos.y > 0 ? Math.Min(PlayerPos.y, 9) : 0;
+
+            if (map[PlayerPos.y, PlayerPos.x] == 2)
+            {
+                PlayerPos = lastPlayerPos;
+            }
+
+
+            var lastEnemyPos = new Position(EnemyPos.x, EnemyPos.y);
             var random = new Random();
-            var processEnemyPosX = playerX - enemyX;
-            var processEnemyPosY = playerY - enemyY;
 
-            if ((playerX == enemyX) && (playerY == enemyY))
-            {
-                return true;
-            }
-            
+            var processEnemyPos = PlayerPos - EnemyPos;
 
-            if (Math.Abs(processEnemyPosX)
-                > Math.Abs(processEnemyPosY))
+            if (PlayerPos.isCollideOtherPos(EnemyPos))
             {
-                enemyX += processEnemyPosX > 0 ? 1 : -1;
+                return 2;
             }
-            else if (Math.Abs(processEnemyPosX) == Math.Abs(processEnemyPosY))
+
+            if (Math.Abs(processEnemyPos.x)
+                > Math.Abs(processEnemyPos.y))
             {
-                switch (random.Next(2))
+                EnemyPos.x += processEnemyPos.x > 0 ? 1 : -1;
+            }
+            else if (Math.Abs(processEnemyPos.x) == Math.Abs(processEnemyPos.y))
+            {
+                if(Math.Abs(processEnemyPos.x) == 1)
                 {
-                    case 0:
-                        enemyY += processEnemyPosY > 0 ? 1 : -1;
-                        break;
-                    case 1:
-                        enemyX += processEnemyPosX > 0 ? 1 : -1;
-                        break;
+                    processEnemyPos = lastPlayerPos - EnemyPos;
+
+                    if (Math.Abs(processEnemyPos.x)
+                > Math.Abs(processEnemyPos.y))
+                    {
+                        EnemyPos.x += processEnemyPos.x > 0 ? 1 : -1;
+                    }
+                    else
+                    {
+                        EnemyPos.y += processEnemyPos.y > 0 ? 1 : -1;
+                    }
+                }
+                else
+                {
+                    switch (random.Next(2))
+                    {
+                        case 0:
+                            EnemyPos.y += processEnemyPos.y > 0 ? 1 : -1;
+                            break;
+                        case 1:
+                            EnemyPos.x += processEnemyPos.x > 0 ? 1 : -1;
+                            break;
+                    }
                 }
             }
             else
             {
-                enemyY += processEnemyPosY > 0 ? 1 : -1;
+                EnemyPos.y += processEnemyPos.y > 0 ? 1 : -1;
             }
 
-            if (map[enemyY, enemyX] == 2)
+            if (map[EnemyPos.y, EnemyPos.x] == 2)
             {
-                enemyX = lastEnemyX;
-                enemyY = lastEnemyY;
+                EnemyPos = lastEnemyPos;
             }
 
-            //플레이어 위치 제한
-            playerX = playerX > 0 ? Math.Min(playerX, 9) : 0;
-            playerY = playerY > 0 ? Math.Min(playerY, 9) : 0;
+            //게임상태 갱신
 
-            if (map[playerY, playerX] == 2)
+            if (map[PlayerPos.y, PlayerPos.x] == 3)
             {
-                playerX = lastPlayerX;
-                playerY = lastPlayerY;
+                return 1;
             }
-
-            return (playerX == enemyX) && (playerY == enemyY);
+            if (PlayerPos.isCollideOtherPos(EnemyPos))
+            {
+                return 2;
+            }
+            return 0;
         }
 
         static void Render()
@@ -169,15 +202,15 @@ namespace _230717
             {
                 for (int x = 0; x < 10; x++)
                 {
-                    if ((playerX == x && playerY == y) && (playerX == enemyX && playerY == enemyY))
+                    if ((PlayerPos.x == x && PlayerPos.y == y) && (PlayerPos.x == EnemyPos.x && PlayerPos.y == EnemyPos.y))
                     {
                         Console.Write("X ");
                     }
-                    else if (playerX == x && playerY == y)
+                    else if (PlayerPos.x == x && PlayerPos.y == y)
                     {
                         Console.Write("P ");
                     }
-                    else if (enemyX == x && enemyY == y)
+                    else if (EnemyPos.x == x && EnemyPos.y == y)
                     {
                         Console.Write("E ");
                     }
@@ -188,6 +221,10 @@ namespace _230717
                     else if (map[y, x] == 2)
                     {
                         Console.Write("! ");
+                    }
+                    else if (map[y,x] == 3)
+                    {
+                        Console.Write("# ");
                     }
                     else
                     {
